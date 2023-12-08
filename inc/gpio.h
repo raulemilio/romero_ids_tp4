71c888c6c58958f5a416b91ae9bcad77e623b7bc
rome <rraulemilioromero@gmail.com>
2023-12-08 14:57:28 -0300

    Estructura inicial del proyecto

---
+++ inc/gpio.h
@@ -0,0 +1,37 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+/*! Estructura anónima del gpio */
+typedef struct gpio_s * gpio_t;
+
+/**
+ * @brief Función constructor del objeto
+ *
+ * @param puerto
+ * @param bit
+ * @return gpio_t
+ */
+gpio_t gpioCreate(uint8_t puerto, uint8_t bit);
+/**
+ * @brief Función set dirección GPIO 
+ *
+ * @param Objeto gpio
+ * @param output
+ * @return void
+ */
+void gpioSetOutput(gpio_t gpio, bool output);
+/**
+ * @brief Función set estado GPIO (alto o bajo)
+ *
+ * @param gpio
+ * @param state
+ * @return void
+ */
+void gpioSetState(gpio_t gpio, bool state);
+/**
+ * @brief Función para leer el estado GPIO (alto o bajo)
+ *
+ * @param gpio
+ * @return bool Estado GPIO
+ */
+bool gpioGetState(gpio_t gpio);
---
+++ src/gpio.c
@@ -0,0 +1,62 @@
+#include "gpio.h"
+#include <stddef.h>
+
+#ifndef GPIO_MAX_INSTANCES
+#define GPIO_MAX_INSTANCES 10
+#endif
+
+/*! Definición de la struct anónima */
+struct gpio_s {
+    uint8_t port; /*!< Puerto GPIO */
+    uint8_t bit;  /*!< Pin GPIO */ 
+    bool output;  /*!< Dirección GPIO */ 
+    bool used;    /*!< Estado de uso de la instancia (en uso o sin uso) */
+};
+
+/**
+ * @brief Función para asignar memoria a una instancia
+ *
+ * @return gpio_t
+ */
+static gpio_t allocateInstance() {
+    static struct gpio_s instances[GPIO_MAX_INSTANCES] = {0};
+
+    gpio_t result = NULL;
+    for (int index = 0; index < GPIO_MAX_INSTANCES; index++) {
+        if (!instances[index].used) {
+            result = &instances[index].used;
+            result->used = true;
+            break;
+        }
+    }
+    return result;
+}
+
+gpio_t gpioCreate(uint8_t puerto, uint8_t bit) {
+#ifdef USE_DYNAMIC_MEM
+    gpio_t self = malloc(sizeof(struct gpio_s));
+#else
+    gpio_t self = allocateInstance();
+#endif
+    if (self) {
+        self->port = puerto;
+        self->bit = bit;
+        self->output = false;
+    }
+    return self;
+}
+
+void gpioSetOutput(gpio_t self, bool output) {
+    self->output = output;
+    HAL_GPIO_SET_OUTPUT(self->port, self->bit);
+}
+
+void gpioSetState(gpio_t self, bool state) {
+    if (self->output) {
+        HAL_GPIO_SET_STATE(self->port, self->bit);
+    }
+}
+
+bool gpioGetState(gpio_t self) {
+    return HAL_GPIO_GET_STATE(self->port, self->bit);
+}
